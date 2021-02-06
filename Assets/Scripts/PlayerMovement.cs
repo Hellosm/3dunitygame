a5a3c5fe7c9e2fa3d263ee2ed30dee5305e1effd
unknown <ryanheron1@icloud.com>
2021-02-06 12:57:57 -0500

    added movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public float speed = 100;
+    public Rigidbody2D rb;
+
+    public void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+    }
+
+}
